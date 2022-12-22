96ef8ace24c1062251adbb615e4bc62a49c08be8
mmhernandez <mmdangelo11@gmail.com>
2022-12-21 19:09:39 -0700

    adding game placement and styling

---
+++ script.js
@@ -0,0 +1,17 @@
+function playGame(element) {
+    //remove play button
+    element.remove();
+
+    //display the game, after the user indicates they want to play
+    wordGame.style.display = "contents";
+
+    displayWordToGuess();
+}
+
+function searchBar(e) {
+    e.preventDefault();
+}
+
+function displayWordToGuess  {
+    
+}
